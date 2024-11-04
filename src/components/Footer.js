fba0c2aa7e4634a47d2eecf0d357dd5893934780
Krish1301 <krishsharma13@gmail.com>
2024-11-04 10:34:16 -0500

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+// Footer.js
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>© 2024 LazyDrobe | All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
