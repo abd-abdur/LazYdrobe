b1dcc4e2dec8498dc064ed3ab0c06d0a9fcf537c
Krish1301 <krishsharma13@gmail.com>
2024-11-04 10:30:49 -0500

    Create installsetup.py

---
+++ installsetup.py
@@ -0,0 +1,8 @@
+npm install react-router-dom
+npm install react-modal
+"scripts": {
+  "start": "react-scripts start",
+  "build": "react-scripts build",
+  "test": "react-scripts test",
+  "eject": "react-scripts eject"
+}
