fad4dcf034d96efdfb9e538183b20c3d5c695264
Abdur Rahman <ar7165@nyu.edu>
2024-11-19 17:17:52 -0500

    working now

--- main.py
+++ main.py
@@ -175,8 +175,6 @@ class WardrobeItemUpdate(BaseModel):
 
     class Config:
         orm_mode = True
-
-//making changes
 class WardrobeItemResponse(WardrobeItemBase):
     item_id: int
     clothing_type: str
