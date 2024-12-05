a8c07ab31e4f36a7bc2ed07beac9518a5ffba9a1
Abdur Rahman <ar7165@nyu.edu>
2024-12-05 15:50:46 -0500

    working on docker

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ app = FastAPI(
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  
+    allow_origins=["*"],  
     allow_credentials=True, allow_methods=["*"],
     allow_headers=["*"],
 )
